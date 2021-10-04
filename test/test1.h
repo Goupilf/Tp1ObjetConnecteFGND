997d7e79dc49af250ee12fd5e0da9377b1662488
NicolasDupuis1335631 <70658853+NicolasDupuis1335631@users.noreply.github.com>
2021-10-04 15:29:35 -0400

    cleaning

--- test/test1.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <unity.h>
-
-void test1(void) {
-    TEST_ASSERT_EQUAL(1, 1);
-}
--- test/test2.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include <unity.h>
-
-void test2(void) {
-    TEST_ASSERT_EQUAL(2, 2);
-}
